f5a16bf095fca625e1a86f04eefe9dd5d50f168b
Sondre Torp <sondrt@uio.no>
2019-04-01 15:10:38 +0300

    Changed a input filename that was wrong.

--- merger.py
+++ merger.py
@@ -10,7 +10,7 @@ import sys
 
 
 foundation_df = pd.read_csv('out_csv_dis.csv',sep=',')
-helvol_geomvol_df = pd.read_csv('helvol_geomvol.csv',sep=',')
+helvol_geomvol_df = pd.read_csv('helvol_geomvol_output.csv',sep=',')
 
 df_new = pd.merge(foundation_df, helvol_geomvol_df, left_on='Charged_ID', right_on='mid')
 df_new = pd.merge(df_new, helvol_geomvol_df, left_on='Discharged_ID', right_on='mid',
