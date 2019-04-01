ecd651f2f0776c7b621ec400b155dbfb7bd9adab
Sondre Torp <sondrt@uio.no>
2019-04-01 15:13:49 +0300

    Removed normvol with very few enteries, added helvolgeomvol

--- prep_csv.py
+++ prep_csv.py
@@ -27,11 +27,7 @@ def main():
     
     l_charged_atoms = True
     l_discharged_atoms= True
-    helvol_charged = False
-    geomvol_charged = False
-    helvol_discharged = False
-    geomvol_discharged = False
-    Target = 'Capacity_Vol'
+    Target = 'E Density Wh/l'
     list_of_predictors = ['Battid',
     # 'Reduced_Cell_Formula',
     # 'Spacegroup',
@@ -41,25 +37,41 @@ def main():
     # 'Specific_E_Wh/kg',
     # 'E Density Wh/l',
     # 'Stability Discharge',
-    # 'Stability Charge'
-    ''
-    ''
-    ''
+    # 'Stability Charge',
+    # 'helvol',
+    'geomvol',
+    # 'helvol_dis',
+    'geomvol_dis'
     ]
+
+
+
     if l_charged_atoms:
     	for ii in [
-    	'F_vol','Ni_vol','O_vol','Cr_vol','La_vol','Mn_vol','As_vol','Mo_vol',
+    	'F_vol','Ni_vol','O_vol','Cr_vol','Mn_vol','As_vol','Mo_vol',
     'Cu_vol','Bi_vol','S_vol','P_vol','V_vol','Fe_vol','Sn_vol','Si_vol',
-    'W_vol','Ti_vol','Co_vol','Sb_vol','Ge_vol','Mg_vol']:
+    'W_vol','Ti_vol','Co_vol','Sb_vol','Ge_vol','Mg_vol'
+    #
+    # 'La_vol'
+    ]:
      	   list_of_predictors.append(ii)
     
     if l_discharged_atoms:
     	for ii in [
-    		'Ge_vol_dis', 'W_vol_dis', 'Ta_vol_dis', 'V_vol_dis', 'Sn_vol_dis', 'Ba_vol_dis', 'P_vol_dis', 
-    'O_vol_dis', 'La_vol_dis', 'Fe_vol_dis', 'Ni_vol_dis', 'Ti_vol_dis', 'N_vol_dis', 'Mg_vol_dis', 
-    'Co_vol_dis', 'F_vol_dis', 'Cr_vol_dis', 'Pr_vol_dis', 'Nb_vol_dis', 'Se_vol_dis', 'Cu_vol_dis', 
-    'Mo_vol_dis', 'Si_vol_dis', 'Mn_vol_dis', 'Bi_vol_dis', 'Sr_vol_dis', 'As_vol_dis', 'Te_vol_dis', 
-    'S_vol_dis', 'Sb_vol_dis']:
+    		'Ge_vol_dis', 'W_vol_dis', 'V_vol_dis', 'Sn_vol_dis', 'Ba_vol_dis', 'P_vol_dis', 
+    'O_vol_dis', 'Fe_vol_dis', 'Ni_vol_dis', 'Ti_vol_dis', 'Mg_vol_dis', 
+    'Co_vol_dis', 'F_vol_dis', 'Cr_vol_dis', 'Cu_vol_dis', 
+    'Mo_vol_dis', 'Si_vol_dis', 'Mn_vol_dis', 'Bi_vol_dis', 'As_vol_dis', 'Te_vol_dis', 
+    'S_vol_dis', 'Sb_vol_dis'
+    #elements with 1 - 3 materials
+    # 'Ta_vol_dis', 
+    # 'Se_vol_dis',
+    # 'Pr_vol_dis',
+    # 'La_vol_dis',
+    # 'Sr_vol_dis',
+    # 'Nb_vol_dis',
+    # 'N_vol_dis'
+    ]:
     		list_of_predictors.append(ii)
     
     
