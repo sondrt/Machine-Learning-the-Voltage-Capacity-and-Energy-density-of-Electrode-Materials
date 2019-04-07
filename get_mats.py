429531ca4fdabe42f410c0ec586f6890abe894d4
Erik Lund <erik.lund@knowit.no>
2019-04-07 22:31:20 +0200

    Put scraping logic into function with some params

--- get_mats.py
+++ get_mats.py
@@ -2,6 +2,17 @@
 
 import requests
 
-for i in range(0, 140):
-    req = 'https://www.materialsproject.org/batteries/search?query={{"working_ion":{{"$in":["Li"]}},"average_voltage":{{"$gte":{0},"$lte":{1}}}}}'.format(-2.1 + i/10.0, -1.9 + i/10.0)
-    print(requests.get(req).text)
+
+def scrape_batteries(working_ion="Mg",
+                     filter_property="average_voltage"):
+    print("Making 14 requests to scrape batteries")
+    print("Working ion: {}".format(working_ion))
+    print("Filtering property: {}".format(filter_property))
+    
+    for i in range(0, 14):
+        req = 'https://www.materialsproject.org/batteries/search?query={{"working_ion":{{"$in":["{ion}"]}},"average_voltage":{{"$gte":{0},"$lte":{1}}}}}' \
+            .format(-2.1 + i / 10.0, -1.9 + i / 10.0, ion=working_ion, property=filter_property)
+        print(requests.get(req).text)
+
+
+scrape_batteries(working_ion="Li")
