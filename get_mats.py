967602e8e99c99f73b5fe48d5ce236997cb60d4a
Erik Lund <erik.lund@knowit.no>
2019-04-18 16:56:39 +0200

    Rename functions and variables. Return a dict of batteries instead of list from initial scrape.

--- get_mats.py
+++ get_mats.py
@@ -5,29 +5,35 @@ import json
 import requests
 
 
-def scrape_batteries(working_ion = "Mg",
-                     filter_property="average_voltage"):
+def scrape_battery_list(working_ion ="Mg",
+                        filter_property="average_voltage"):
     print("Making 14 requests to scrape batteries")
     print("Working ion: {}".format(working_ion))
     print("Filtering property: {}".format(filter_property))
 
-    results: List[Dict] = []
+    battery_materials = {}
 
-    for i in range(0, 30):
+    for i in range(0, 15):
         url = 'https://www.materialsproject.org/batteries/search?query={{"working_ion":{{"$in":["{ion}"]}},"average_voltage":{{"$gte":{0},"$lte":{1}}}}}' \
             .format(-2.1 + i / 10.0, -1.9 + i / 10.0, ion=working_ion, property=filter_property)
 
         response = requests.get(url)
+        response_batteries_list = response.json()
 
         assert response.status_code == 200
-        assert not isinstance(response.json(), dict)
+        assert isinstance(response_batteries_list, list)
+        assert not isinstance(response_batteries_list, dict)
 
-        results.extend(response.json())
+        for battery in response_batteries_list:
+            battery_materials[battery["battid"]] = battery
 
-        print("len(results))
+        print(len(battery_materials))
+        print(len(set(battery_materials)))
 
-    return results
+    return list(battery_materials.values())
 
 
-print(scrape_batteries(working_ion="Li"))
-# scrape_batteries(working_ion="Mg")
+li_batteries = scrape_battery_list(working_ion="Li")
+
+print(li_batteries)
+# mg_batteries = scrape_batteries(working_ion="Mg")
